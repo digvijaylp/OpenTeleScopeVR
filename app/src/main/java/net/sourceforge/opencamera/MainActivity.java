d4ff6853e83270a773167affb0c84ce6f21d50da
digvijaylp <digvijay.l.prakash@outlook.com>
2026-08-13 16:15:54 +0530

    clean up

--- app/src/main/java/net/sourceforge/opencamera/MainActivity.java
+++ app/src/main/java/net/sourceforge/opencamera/MainActivity.java
@@ -212,8 +212,8 @@ public class MainActivity extends AppCompatActivity implements PreferenceFragmen
     private float mWaterDensity = 1.0f;
 
     // whether to lock to landscape orientation, or allow switching between portrait and landscape orientations
-    //public static final boolean lock_to_landscape = true;
-    public static final boolean lock_to_landscape = false;
+    public static final boolean lock_to_landscape = true;
+    //public static final boolean lock_to_landscape = false;
 
     // handling for lock_to_landscape==false:
 
